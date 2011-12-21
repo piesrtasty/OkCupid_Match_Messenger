899a55358fd7c7f65b5fa2cade133c4f548b9e14
virtually-ghetto <lukemhamilton@gmail.com>
2011-12-21 14:44:29 -0500

    Auto login working, navigation to matches page working

--- okcupid_match_messenger.rb
+++ okcupid_match_messenger.rb
@@ -2,3 +2,26 @@ require 'safariwatir'
 
 # Create a new browser object
 browser = Watir::Safari.new
+# Specify the OkCupid URL
+url = "http://www.okcupid.com"
+
+# Navigate the browser to okcupid.com
+browser.goto url
+
+# Enter the username
+browser.text_field(:id, "sidebar_signin_username").set "username"
+# Enter the password
+browser.password(:id, "sidebar_signin_password").set "password"
+
+# Click the signin button
+browser.form(:id, "sidebar_signin_form").submit
+
+# Get all links on the page
+links = browser.links
+
+# Find the link with text "Matches"
+links.each do |link|
+  if link.text == "Matches"
+    link.click
+  end
+end
