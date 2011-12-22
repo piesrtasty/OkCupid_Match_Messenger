62993932b77a369315e8fef0d4f44b9c2073384a
virtually-ghetto <lukemhamilton@gmail.com>
2011-12-21 22:34:13 -0500

    got message dialog popping up

--- okcupid_match_messenger.rb
+++ okcupid_match_messenger.rb
@@ -27,23 +27,48 @@ links.each do |link|
 end
 
 # Sleep the browser to manually do infinite scroll
-sleep 10
+# sleep 10
+# puts "Done Sleeping"
 
-puts "Done Sleeping"
+span_count = 0
 
-link_count = 0
+usernames = []
 
-profile_links = []
+all_spans = browser.spans
 
-all_links = browser.links
-
-all_links.each do |al|
-  link_count += 1
-  puts al.class_name
-
+all_spans.each do |span|
+  span_count += 1
+  if span.class_name == "username"
+    usernames << span.text
+  end
 end
+
+profile_url = "http://www.okcupid.com/profile/" + usernames[0] + "?cf=regular"
+
+browser.goto profile_url
+
+if browser.link(:id, "match-perc-stoplight-red").exists?
+  puts "we got a red"
+  browser.link(:id, "match-perc-stoplight-red").click()
+elsif browser.link(:id, "match-perc-stoplight-yellow").exists?
+  puts "we got a yellow"
+  browser.link(:id, "match-perc-stoplight-yellow")
+elsif browser.link(:id, "match-perc-stoplight-green").exists?
+  puts "we got a green"
+  browser.link(:id, "match-perc-stoplight-green")
+end
+
+
+
+# match-perc-stoplight-red
+# match-perc-stoplight-yellow
+# match-perc-stoplight-yellow
+
+# http://www.okcupid.com/profile/BaileyGinNYC?cf=regular
  
-puts count
+puts span_count
+puts ""
+puts usernames
 
 # Attempt 1
 
